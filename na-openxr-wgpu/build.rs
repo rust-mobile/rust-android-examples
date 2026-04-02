7b42ef8556bbc01b881a0ee7c7545e1c70308fb0
Robert Bragg <robert@sixbynine.org>
2026-04-02 18:46:16 +0100

    fix cargo fmt in na-openxr-wgpu/build.rs

--- na-openxr-wgpu/build.rs
+++ na-openxr-wgpu/build.rs
@@ -10,7 +10,6 @@ fn main() {
             }
         };
 
-
         let libdir = std::path::Path::new(&std::env::var("CARGO_MANIFEST_DIR").unwrap())
             .join(format!("app/src/main/jniLibs/{android_abi}"));
         println!("cargo:rustc-link-search={}", libdir.to_string_lossy());
